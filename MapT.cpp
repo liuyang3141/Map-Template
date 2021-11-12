b3e7bfe0d780d727c9f6d0a492c82d3983fb7c33
liuyang3141 <36750588+liuyang3141@users.noreply.github.com>
2021-11-12 10:10:31 -0800

    Initial commit

---
+++ MapT.cpp
@@ -0,0 +1,19 @@
+
+
+#include "MapT.h"
+#include <unordered_map>
+//using namespace std;
+
+const int DEFAULT_BUCKETS = 10;
+const double DEFAULT_LOAD = 1.0;
+
+
+template<class K, class T>
+int MapT<K, T>::GetHashIndex(const K &key) {
+    unordered_map<K,T> mapper;
+    typename unordered_map<K,T>::hasher hashFunction = mapper.hash_function();
+    return static_cast<int>(hashFunction(key) % numBuckets);
+}
+
+
+
